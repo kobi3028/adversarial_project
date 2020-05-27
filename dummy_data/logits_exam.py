c945bb07a8c374fc9c03286e895145f1c3dcedb4
Yaakov Cohen <Yaakov1.Cohen@intel.com>
2020-05-27 08:43:06 +0300

    add dummy_data folder

---
+++ dummy_data/logits_exam.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+#creates logits examples
+number_of_examples = 20
+number_of_layers = 18
+layer_length = 256
+layer_width = 256
+lowest_y_value = 0
+highest_y_value = 9
+
+logits_on_x_benign = np.random.rand(number_of_examples, number_of_layers, layer_length, layer_width)
+logits_on_x_adv = np.random.rand(number_of_examples, number_of_layers, layer_length, layer_width)
+y_benign = np.random.randint(0, high=10, size=(number_of_examples))
+y_adv_original = np.random.randint(lowest_y_value, high=(highest_y_value+1), size=(number_of_examples))
+y_adv_target = np.random.randint(lowest_y_value, high=(highest_y_value+1), size=(number_of_examples))
+
+np.save(r'./data/logits_on_x_benign', logits_on_x_benign)
+np.save(r'./data/logits_on_x_adv',    logits_on_x_adv)
+np.save(r'./data/y_benign',           y_benign)
+np.save(r'./data/y_adv_original',     y_adv_original)
+np.save(r'./data/y_adv_target',       y_adv_target)
